af9d12cfc73f917151837e1924c3d5597bc3ff39
NB1355 <nahal5513@gmail.com>
2023-08-12 01:16:02 +0000

    FIX

--- veriwiseweb/settings.py
+++ veriwiseweb/settings.py
@@ -134,8 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT= (os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]= "static/"
+
+
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
